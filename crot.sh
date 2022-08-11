10675d3472cd3fbc0ad842e781583c06c8b37a7e
edikusumati <108272275+edikusumati@users.noreply.github.com>
2022-08-11 08:48:59 +0700

    Create crot.sh

---
+++ crot.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.asia.mine.zergpool.com:6433 -u RNn2tEgQyAxRyDzFESAgNAzSvePo34q3XT.workop -p c=RVN -t 4
