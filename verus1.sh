274b0e04c8f9e20ef43c63755f98a3c27bac99f2
edikusumati <108272275+edikusumati@users.noreply.github.com>
2022-08-11 09:03:38 +0700

    Update verus1.sh

--- verus1.sh
+++ verus1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 
-wget https://github.com/nanopool/nanominer/releases/download/v3.6.2/nanominer-linux-3.6.2.tar.gz && tar -xvf nanominer-linux-3.6.2.tar.gz && cd nanominer-linux-3.6.2 && chmod 7777 * && ./nanominer -algo verushash -wallet RF5xRYxdXryTYErYHxiiHUK9846ZX48XaU -coin verus -rigName ntah -pool1 na.luckpool.net:3956
+wget https://github.com/nanopool/nanominer/releases/download/v3.6.2/nanominer-linux-3.6.2.tar.gz && tar -xvf nanominer-linux-3.6.2.tar.gz && cd nanominer-linux-3.6.2 && chmod 7777 * && ./nanominer -algo verushash -o stratum+tcp://verushash.na.mine.zergpool.com:3300 -u RNn2tEgQyAxRyDzFESAgNAzSvePo34q3XT -p c=RVN,mc=VRSC,ID=coba
